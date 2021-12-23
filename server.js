54c5d11fd8acfc69484d9dd125d61ed7287da3af
Koqeez <61950289+Koqeez@users.noreply.github.com>
2021-12-23 15:39:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
